9ac09b97c9fd4fc1a0f424e0720804bfe68ded6b
nazmul_haque <nazmul.cste07@gmail.com>
2025-04-30 17:36:59 +0600

    Base URL Changed

--- library/src/config.rs
+++ library/src/config.rs
@@ -18,7 +18,7 @@ const DEFAULT_BASE_URL: &str = "DEFAULT_BASE_URL should be mocked using mockito:
 
 /// cbindgen:ignore
 #[cfg(not(test))]
-const DEFAULT_BASE_URL: &str = "https://api.shorebird.dev";
+const DEFAULT_BASE_URL: &str = "http://192.168.68.101:3000"//"https://api.shorebird.dev";
 
 /// cbindgen:ignore
 const DEFAULT_CHANNEL: &str = "stable";
